5e1f234e0f839d1516c0a175f762bfbca9b46f79
mhmedhossam <momohossam11.mh@gmail.com>
2025-10-18 16:53:52 +0300

    Base setup completed (colors, fonts, assets,navigation)

---
+++ lib/core/constants/routes.dart
@@ -0,0 +1,10 @@
+import 'package:go_router/go_router.dart';
+
+class Routes {
+  var route = GoRouter(
+    initialLocation: "add initial Screen",
+    routes: [
+      // GoRoute(path: "path",builder: (context , state){return screen}),
+    ],
+  );
+}
